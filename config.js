8ac1d99c1c6d5aedef812607f7a33d71a5ca2d7a
connradolisboa <connradolisboa@gmail.com>
2025-03-30 23:05:09 -0300

    commit

--- config.js
+++ config.js
@@ -1,17 +1,7 @@
 const config = {
-  obsidianVault: "/Users/alex/library/Mobile Documents/iCloud~md~obsidian/Documents/ZazenCodes/notes",
+  obsidianVault: "/Users/connrado/Second Brain",
   noteTagIds: [
-    { folder: "book", databaseId: "b403753ce0c049e48c83ff2c3d896843" },
-    { folder: "blog", databaseId: "1f9ab538edc04cb19e876653040b761f" },
-    { folder: "course", databaseId: "ccf45222ea024f3795d2df10340afcef" },
-    { folder: "doc", databaseId: "4193377febff45b1a6cc587df87bf424" },
-    { folder: "fact", databaseId: "49b474595a4b4893b3f23d68a2ad4709" },
-    { folder: "resource", databaseId: "de8db5301bb44491a807a72d0775fb2e" },
-    { folder: "video", databaseId: "46af621096a145908bb04054629ee072" },
-    { folder: "cheat-sheet", databaseId: "9fd1106079a94a76a3834472bc555fd6" },
-    { folder: "code-snip", databaseId: "445273a8fd1e43b2a4c1e8a0adda0a28" },
-    { folder: "prompt", databaseId: "3cfbd55c8c7642fba9b8d0500c22cd0d" },
-    { folder: "podcast", databaseId: "7bab372015af4287a40eb61c7bb16aa3" },
+    { folder: "Management/Pieces", databaseId: "b403753ce0c049e48c83ff2c3d896843 a54888e0f73042a68bc9fec5fc8985f7"},
   ]
 }
 
